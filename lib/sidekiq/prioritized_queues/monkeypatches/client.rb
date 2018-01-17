33fb145e1478a84bf2f78b1ce908519e3e233e5d
Steven Weller <github@suweller.com>
2018-01-17 14:59:18 +0100

    Align monkey patch to sidekiq 5
    the to_push section was not used and the assignment shadowed on a few
    lines below.

--- lib/sidekiq/prioritized_queues/monkeypatches/client.rb
+++ lib/sidekiq/prioritized_queues/monkeypatches/client.rb
@@ -1,21 +1,18 @@
+# frozen_string_literal: true
+
 module Sidekiq
   class Client
-
   private
 
     def atomic_push(conn, payloads)
-      if payloads.first['at']
+      if payloads.first['at'.freeze]
         conn.zadd('schedule'.freeze, payloads.map do |hash|
           at = hash.delete('at'.freeze).to_s
           [at, Sidekiq.dump_json(hash)]
         end)
       else
-        q = payloads.first['queue']
+        q = payloads.first['queue'.freeze]
         now = Time.now.to_f
-        to_push = payloads.map do |entry|
-          entry['enqueued_at'.freeze] = now
-          Sidekiq.dump_json(entry)
-        end
         conn.sadd('queues'.freeze, q)
         payloads.each do |entry|
           to_push  = Sidekiq.dump_json(entry)
