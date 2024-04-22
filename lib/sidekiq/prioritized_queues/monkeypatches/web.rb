08218dde7b95115942a975c4b161b8a44f9a7997
Martiniano Felici <martiniano.felici@gmail.com>
2024-04-22 18:04:06 +0200

    Fix queue show controller action for Sidekiq Web

--- lib/sidekiq/prioritized_queues/monkeypatches/web.rb
+++ lib/sidekiq/prioritized_queues/monkeypatches/web.rb
@@ -3,17 +3,19 @@ require 'sidekiq/web'
 
 module Sidekiq
   class WebApplication
-    get '/queues/:name' do
+    custom_block = Proc.new do
       @name = route_params[:name]
 
       halt(404) if !@name || @name !~ QUEUE_NAME
 
       @count = (params["count"] || 25).to_i
       @queue = Sidekiq::Queue.new(@name)
-      (@current_page, @total_size, @messages) = page("queue:#{@name}", params["page"], @count, reverse: params["direction"] == "asc")
-      @messages = @messages.map { |msg, _priority| Sidekiq::JobRecord.new(msg, @name) }
+      (@current_page, @total_size, @jobs) = page("queue:#{@name}", params["page"], @count, reverse: params["direction"] == "asc")
+      @jobs = @jobs.map { |msg, _priority| Sidekiq::JobRecord.new(msg, @name) }
 
       erb(:queue)
     end
+
+    @routes[WebRouter::GET].unshift WebRoute.new(WebRouter::GET, '/queues/:name', custom_block)
   end
 end
