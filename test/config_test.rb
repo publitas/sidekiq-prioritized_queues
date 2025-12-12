f668506a732b50c2a8e7edcb2de75a5bf937d0a2
Rodrigo Soares <rodrigo@publitas.com>
2025-12-12 16:12:20 -0300

    Fix queue name in spec

--- test/config_test.rb
+++ test/config_test.rb
@@ -6,7 +6,7 @@ module Sidekiq
       it 'should load non prioritized queues from config file' do
         config = { require: File.join(__dir__, 'fixtures') }
         gem_config = Sidekiq::PrioritizedQueues::Config.new(config)
-        assert_equal %w[ignored_queue], gem_config.non_prioritized_queues
+        assert_equal %w[non_prio], gem_config.non_prioritized_queues
       end
 
       it 'should return empty array if no ignored queues are set' do
